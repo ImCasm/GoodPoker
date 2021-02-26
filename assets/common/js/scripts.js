45d00ccd4eb25412dc56b55fcb2c51d5044dfa56
Cristian Sánchez <cristian.asm19@gmail.com>
2021-02-26 13:48:15 -0500

    entrega final

--- assets/common/js/scripts.js
+++
@@ -1,5 +0,0 @@
-// Steper interaction
-$(document).on("click", "a.link-steper-list", function () {
-  $("a.link-steper-list").parent("li").removeClass("active");
-  $("a.active").parent("li").addClass("active");
-});
